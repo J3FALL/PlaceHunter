1f0c6abca34107dd1c0d0abd7a89f29a50fd3e9b
J3FALL <crisp.coad@gmail.com>
2016-01-06 14:52:39 +0400

    add basic lifetimePickerDialog

--- app/src/main/java/pavel/rdtltd/placehunter/ui/createmarker/CreateMarkerActivity.java
+++ app/src/main/java/pavel/rdtltd/placehunter/ui/createmarker/CreateMarkerActivity.java
@@ -1,16 +1,20 @@
 package pavel.rdtltd.placehunter.ui.createmarker;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatSeekBar;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,11 +22,15 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.NumberPicker;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.codetroopers.betterpickers.hmspicker.HmsPickerBuilder;
+import com.codetroopers.betterpickers.numberpicker.NumberPickerBuilder;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -41,6 +49,7 @@ import retrofit.Retrofit;
 public class CreateMarkerActivity extends AppCompatActivity {
 
     private ImageButton back;
+    private LinearLayout picker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +59,6 @@ public class CreateMarkerActivity extends AppCompatActivity {
         //Typeface typeface = Typeface.createFromAsset(getAssets(), "font/Roboto-Thin.ttf");
 
 
-        back = (ImageButton) findViewById(R.id.back);
 
         bindViews();
         setOnClickListeners();
@@ -65,10 +73,68 @@ public class CreateMarkerActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        picker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showLifetimeDialog();
+            }
+        });
     }
 
     private void bindViews() {
         back = (ImageButton) findViewById(R.id.back);
+        picker = (LinearLayout) findViewById(R.id.picker);
+    }
+
+    private void showLifetimeDialog() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AppTheme_DialogStyle);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.lifetime_dialog, null);
+
+        final TextView hourView, weekView, monthView;
+        hourView = (TextView) view.findViewById(R.id.hourView);
+        weekView = (TextView) view.findViewById(R.id.weekView);
+        monthView = (TextView) view.findViewById(R.id.monthView);
+
+        hourView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hourView.setTextColor(getResources().getColor(R.color.dialog_text_pressed));
+                weekView.setTextColor(getResources().getColor(R.color.dialog_text_unpressed));
+                monthView.setTextColor(getResources().getColor(R.color.dialog_text_unpressed));
+            }
+        });
+        weekView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hourView.setTextColor(getResources().getColor(R.color.dialog_text_unpressed));
+                weekView.setTextColor(getResources().getColor(R.color.dialog_text_pressed));
+                monthView.setTextColor(getResources().getColor(R.color.dialog_text_unpressed));
+            }
+        });
+        monthView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hourView.setTextColor(getResources().getColor(R.color.dialog_text_unpressed));
+                weekView.setTextColor(getResources().getColor(R.color.dialog_text_unpressed));
+                monthView.setTextColor(getResources().getColor(R.color.dialog_text_pressed));
+            }
+        });
+        builder.setView(view)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        builder.show();
     }
     /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
