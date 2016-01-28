b1e58332a9beb8a416103dee8f7b74e0ccf691ce
J3FALL <crisp.coad@gmail.com>
2016-01-28 13:34:55 +0700

    add toolbar to MarkerInfoActivity

--- app/src/main/java/pavel/rdtltd/placehunter/ui/MarkerInfoActivity.java
+++ app/src/main/java/pavel/rdtltd/placehunter/ui/MarkerInfoActivity.java
@@ -2,6 +2,9 @@ package pavel.rdtltd.placehunter.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import pavel.rdtltd.placehunter.R;
 
@@ -10,9 +13,43 @@ import pavel.rdtltd.placehunter.R;
  */
 public class MarkerInfoActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.custom_marker);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            toolbar.setTitle(R.string.info_marker_activity_title);
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.basic_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: {
+                finish();
+            }
+            case R.id.share: {
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
