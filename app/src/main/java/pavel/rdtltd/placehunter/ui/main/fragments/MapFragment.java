a5cbaff52d3603847b9d1f51f1e08f0a6e018612
J3FALL <crisp.coad@gmail.com>
2016-02-03 10:51:53 +0700

    update infowindow layout

--- app/src/main/java/pavel/rdtltd/placehunter/ui/main/fragments/MapFragment.java
+++ app/src/main/java/pavel/rdtltd/placehunter/ui/main/fragments/MapFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -102,6 +103,9 @@ public class MapFragment extends android.support.v4.app.Fragment{
                     @Override
                     public View getInfoContents(Marker marker) {
                         View v = getLayoutInflater(savedInstanceState).inflate(R.layout.custom_infowindow, null);
+
+                        TextView title = (TextView) v.findViewById(R.id.title);
+                        title.setText(clickedMarker.getTitle());
                         return v;
                     }
                 });
